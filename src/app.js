9ec7e42652705e18f74259348c645dafd21a4cb1
Alexandre HEBERT <ahebert@xebia.fr>
2015-06-11 19:59:11 +0200

    added event listening : from filter to app

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
-import {ComponentAnnotation as Component, ViewAnnotation as View, bootstrap} from 'angular2/angular2';
+import {ComponentAnnotation as Component, ViewAnnotation as View, Event, bootstrap} from 'angular2/angular2';
 import {Slots} from 'slots';
+import {Filter} from 'filter';
 
 @Component({
     selector: 'xke-app'
 })
 @View({
-    template: `<h1>{{title}}</h1><hr /><xke-filters></xke-filters><xke-slots></xke-slots>`,
-    directives: [Slots]
+    template: `
+        <h1>{{title}}</h1>
+        <hr />
+        <xke-filter (filter)="onFilter($event)"></xke-filter>
+        <p>filter value = {{ value }}</p>
+        <xke-slots></xke-slots>
+    `,
+    directives: [Slots, Filter]
 })
 export class App {
     title:string = 'App XVote';
+    value:string = '';
+
     constructor() {
+    }
+
+    onFilter(event:Event) {
+        this.value = event.value;
     }
 }
 
---
+++ src/filter.js
@@ -0,0 +1,21 @@
+import {ComponentAnnotation as Component, ViewAnnotation as View, EventEmitter} from 'angular2/angular2';
+import {SlotModel} from 'slot-store'
+
+@Component({
+    selector: 'xke-filter',
+    events: ['filter']
+})
+@View({
+    templateUrl: 'filter.html'
+})
+export class Filter {
+    filter:EventEmitter;
+
+    constructor() {
+        this.filter = new EventEmitter();
+    }
+
+    onFilter(filter:string) {
+        this.filter.next({value:filter});
+    }
+}
