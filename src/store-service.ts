bdf13f08a86f664cf2c588f3e3c9ac7e0f3b6a6c
dimapod <dimapod@gmail.com>
2015-10-17 21:23:27 +0200

    Maj slot data

--- src/store-service.ts
+++ src/store-service.ts
@@ -2,18 +2,20 @@ import {Http} from 'angular2/http';
 
 export class StoreService {
     slots:Array<any> = [
-        { "title": "Rising Architect", "speakers": ["Pablo Lopez", "Xavier Bucchiotty"], "attendees": 15 },
-        { "title": "Build rock SOLID code", "speakers": ["Clément Heliou", "Ilja Kempf", "Pierre-Jean Vardanega", "Sarah Buisson"], "attendees": 5 },
-        { "title": "What's new at Google I/O and WWDC", "speakers": ["Jérémie Martinez", "Simone Civetta"], "attendees": 7 },
-        { "title": "Scaling Culture - L'agilité à grande échelle en vraie", "speakers": ["Nicolas Lochet"], "attendees": 9 },
-        { "title": "Présentation de CloudUnit, un PaaS Java s'appuyant sur Docker développé par Treeptik", "speakers": ["Jean-Louis Rigau"], "attendees": 2 },
-        { "title": "Le cadrage Agile timeboxé", "speakers": ["Gwénaël Bonhommeau"], "attendees": 1 },
-        { "title": "Breizhcamp et la suite", "speakers": ["Sameh Ben Fredj"], "attendees": 17 },
-        { "title": "Rex : LV Live", "speakers": ["David Caramelo"], "attendees": 5 },
-        { "title": "Brace yourselves, Angular 2 is comming !", "speakers": ["Alexandre Hebert", "Dmytro Podyachiy"], "attendees": 14 },
-        { "title": "BDD in action by exemple for dummies", "speakers": ["Clément Rochas", "Diego Lemos"], "attendees": 8 },
-        { "title": "iM-1 / The Mower World & First AI", "speakers": ["Robert Duchoux"], "attendees": 9 },
-        { "title": "Docker Swarm", "speakers": ["Ivan Beauvais","Jean-Eudes Couignoux","Jean-Louis Rigau","Jonathan Raffre","Roksolana Ivanyshyn"], "attendees": 1 }
+        { "title": "Be data driven : monter son data lab", "speakers": ["Julien Buret"], "attendees": 15 },
+        { "title": "Apache mesos, ou comment exploiter les ressources de votre data center", "speakers": ["Jean-pascal Thiery"], "attendees": 5 },
+        { "title": "Swift dans la vraie vie", "speakers": ["Simone Civetta", "Fabien Mirault"], "attendees": 7 },
+        { "title": "Les algorithmes du machine learning", "speakers": ["Alban Phélip", "Yoann Benoit"], "attendees": 9 },
+        { "title": "Dessine moi un nuage", "speakers": ["Aurélien maury", "Séven le Mesle"], "attendees": 2 },
+        { "title": "Kafka par la face nord", "speakers": ["Xavier Bucchiotty", "Matthieu Blanc"], "attendees": 1 },
+        { "title": "Le nouvel ops", "speakers": ["Séven le Mesle"], "attendees": 17 },
+        { "title": "Du javascript au métal, le web «natif»", "speakers": ["Paali Tandia"], "attendees": 5 },
+        { "title": "A la découverte des mécanismes internes de cassandra", "speakers": ["Matthieu Nantern"], "attendees": 14 },
+        { "title": "Cqrs + eventsourcing : en direct !", "speakers": ["Pierre-jean Vardanega", "Sebastian le Merdy"], "attendees": 8 },
+        { "title": "Scaling culture", "speakers": ["Nicolas Lochet"], "attendees": 9 },
+        { "title": "Double loop tdd", "speakers": ["Pierre-jean Vardanega", "Diego lemos"], "attendees": 6 },
+        { "title": "Brace yourselves, angular 2 is coming!", "speakers": ["Alexandre Hebert", "Dmytro Podyachiy"], "attendees": 14 },
+        { "title": "Le challenge des microservices", "speakers": ["Nicolas Jozwiak"], "attendees": 1 }
     ];
 
     getSlots() {
