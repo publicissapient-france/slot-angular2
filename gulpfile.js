96e8774b79fd019d89c8076e9f4061f686215f15
Alexandre HEBERT <ahebert@xebia.fr>
2015-10-18 15:02:03 +0200

    fixed @Inject and add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,73 @@
 var gulp = require('gulp');
 
 var PATHS = {
-  src: {
-    ts: 'src/**/*.ts',
-    assets: ['src/**/*.html', 'src/**/*.json'],
-    less: 'styles/main.less'
-  },
-  libs: [
-    'node_modules/systemjs/dist/system-csp-production.js',
-    'node_modules/angular2/bundles/angular2.min.js',
-    'node_modules/angular2/bundles/router.dev.min.js',
-    'node_modules/angular2/bundles/http.min.js',
-    'node_modules/less/dist/less.min.js'
-  ]
+    src: {
+        ts: 'src/**/*.ts',
+        assets: ['src/**/*.html', 'src/**/*.json'],
+        less: 'styles/main.less'
+    },
+    libs: [
+        'node_modules/systemjs/dist/system-csp-production.js',
+        'node_modules/angular2/bundles/angular2.min.js',
+        'node_modules/angular2/bundles/router.dev.min.js',
+        'node_modules/angular2/bundles/http.min.js',
+        'node_modules/less/dist/less.min.js'
+    ]
 };
 
 gulp.task('clean', function (done) {
-  var del = require('del');
-  del(['dist'], done);
+    var del = require('del');
+    del(['dist'], done);
 });
 
 gulp.task('ts2js', function () {
-  var typescript = require('gulp-typescript');
-  var tsResult = gulp.src(PATHS.src.ts)
-    .pipe(typescript({
-      noImplicitAny: true,
-      module: 'system',
-      target: 'ES5',
-      moduleResolution: 'node',
-      emitDecoratorMetadata: true,
-      experimentalDecorators: true
-    }));
+    var typescript = require('gulp-typescript');
+    var sourcemaps = require('gulp-sourcemaps');
 
-  return tsResult.js.pipe(gulp.dest('dist'));
+    var tsResult = gulp.src(PATHS.src.ts)
+        .pipe(sourcemaps.init())
+        .pipe(typescript({
+            noImplicitAny: true,
+            module: 'system',
+            target: 'ES5',
+            moduleResolution: 'node',
+            emitDecoratorMetadata: true,
+            experimentalDecorators: true
+        }))
+        .js
+        .pipe(sourcemaps.write());
+
+    return tsResult.pipe(gulp.dest('dist'));
 });
 
 gulp.task('serve', ['default'], function () {
-  var http = require('http');
-  var connect = require('connect');
-  var serveStatic = require('serve-static');
+    var http = require('http');
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
 
-  var port = 9000, app;
+    var port = 9000, app;
 
-  gulp.watch(PATHS.src.assets, ['assets']);
-  gulp.watch(PATHS.src.ts, ['ts2js']);
-  gulp.watch(PATHS.src.less, ['styles']);
+    gulp.watch(PATHS.src.assets, ['assets']);
+    gulp.watch(PATHS.src.ts, ['ts2js']);
+    gulp.watch(PATHS.src.less, ['styles']);
 
-  app = connect().use(serveStatic(__dirname + '/dist'));
-  http.createServer(app).listen(port);
+    app = connect().use(serveStatic(__dirname + '/dist'));
+    http.createServer(app).listen(port);
 });
 
-/*gulp.task('js', function () {
- return gulp.src(PATHS.src.js)
- .pipe(rename({extname: ''})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
- .pipe(plumber())
- .pipe(traceur({
- modules: 'instantiate',
- moduleName: true,
- annotations: true,
- types: true,
- memberVariables: true
- }))
- .pipe(rename({extname: '.js'})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
- .pipe(gulp.dest('dist'));
- });*/
-
 gulp.task('assets', function () {
-  return gulp.src(PATHS.src.assets)
-    .pipe(gulp.dest('dist'));
+    return gulp.src(PATHS.src.assets)
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('styles', function () {
-  return gulp.src(PATHS.src.less)
-    .pipe(gulp.dest('dist'));
+    return gulp.src(PATHS.src.less)
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('libs', function () {
-  return gulp.src(PATHS.libs)
-    .pipe(gulp.dest('dist/lib'));
+    return gulp.src(PATHS.libs)
+        .pipe(gulp.dest('dist/lib'));
 });
 
 gulp.task('default', ['ts2js', 'libs', 'assets', 'styles']);
--- src/store-service.ts
+++ src/store-service.ts
@@ -1,10 +1,11 @@
 import {Http, Response} from 'angular2/http';
-import {Inject} from 'angular2/core';
+import {Injectable} from 'angular2/core';
 
+@Injectable()
 export class StoreService {
     slots:Array<any> = [];
 
-    constructor(@Inject http: Http) {
+    constructor(http: Http) {
         http.get('slots.json')
             .map((res:Response) => res.json())
             .subscribe((value:Array<any>) => this.slots = value);
